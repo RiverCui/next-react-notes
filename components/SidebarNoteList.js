03662af2bbb50c3d630cf6c474d3378f8c6c48b6
River <rivertsui@outlook.com>
2024-02-17 15:28:37 +0800

    sidebar search feature

--- components/SidebarNoteList.js
+++ components/SidebarNoteList.js
@@ -1,25 +1,30 @@
-import SidebarNoteItem from '@/components/SidebarNoteItem'
+import SidebarNoteListFilter from '@/components/SidebarNoteListFilter'
+import SidebarNoteItemHeader from '@/components/SidebarNoteItemHeader'
 import { getAllNotes } from '@/lib/redis'
 import { sleep } from '@/lib/utils'
 
 export default async function NoteList() {
   // 模拟网络延迟
-  await sleep(5000)
+  await sleep(3000)
   const notes = await getAllNotes()
 
-  const arr = Object.entries(notes)
-
-  if(arr.length == 0) {
+  if (Object.entries(notes).length === 0) {
     return <div className="notes-empty">
       {'No notes yet'}
     </div>
   }
 
-  return <ul className="notes-list">
-    {arr.map(([noteId, note]) => {
-      return <li key={noteId}>
-        <SidebarNoteItem noteId={noteId} note={JSON.parse(note)} />
-      </li>
-    })}
-  </ul>
+  return (
+    <SidebarNoteListFilter notes={
+      Object.entries(notes).map(([noteId, note]) => {
+        const noteData = JSON.parse(note)
+        return {
+          noteId,
+          note: noteData,
+          header: <SidebarNoteItemHeader title={noteData.title} updateTime={noteData.updateTime} />
+        }
+      })
+    }>
+    </SidebarNoteListFilter>
+  )
 }
